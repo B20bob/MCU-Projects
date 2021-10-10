71ce984b9f8653df1ae7ef3d45bf50bf3e38f4a8
csn495 <calvinn495@gmail.com>
2021-10-09 20:33:29 -0400

    added folder for raspi + bme test

---
+++ Raspberry-Pi/BME280/RPLCD-BME280.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+from RPLCD import i2c
+from time import sleep
+import time
+import board
+from adafruit_bme280 import basic as adafruit_bme280
+
+# constants to initialise the LCD
+lcdmode = 'i2c'
+cols = 20
+rows = 4
+charmap = 'A00'
+i2c_expander = 'PCF8574'
+
+# Generally 27 is the address;Find yours using: i2cdetect -y 1 
+address = 0x27 
+port = 1 # 0 on an older Raspberry Pi
+
+# Initialise the LCD
+lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
+                  cols=cols, rows=rows)
+
+#Beginning of my code
+
+# Create sensor object, using the board's default I2C bus.
+i2c = board.I2C()  # uses board.SCL and board.SDA
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
+
+# change this to match the location's pressure (hPa) at sea level
+bme280.sea_level_pressure = 1010.0
+
+while True:
+
+    def read_temp():
+        tempC = bme280.temperature
+        temp = tempC * 9/5.0 + 32
+        temp = str(round(temp, 2))
+        temp = str(temp)
+        return temp
+
+    def read_humidity():
+        relative_humidity = bme280.relative_humidity
+        humidity = str(round(relative_humidity, 2))
+        humidity = str(humidity)
+        return humidity
+    
+    def read_baro_pressure():
+        pressure = bme280.pressure
+        pressure = str(round(pressure, 2))
+        pressure = str(pressure)
+        return pressure
+
+
+    
+    print(read_temp())
+    print(read_humidity())
+    print(read_baro_pressure())
+    sleep(3)
